ca945f4ab56fee8f5d85af0d0731ff3a698e7d29
assumptionsandg <jrhutchinson01@gmail.com>
2020-03-11 17:21:48 +0000

    Server issue fixed

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-var mongojs = require("mongojs");
-//var db = mongojs('localhost:27017/projectUltra', ['account','progress']);
-
 var express = require('express');
 var exp = express();
 var serv = require('http').Server(exp);
-
-var io = require('socket.io').listen(8080); 
 
 //server
 
 exp.get('/',function(req, res) {
     res.sendFile(__dirname + '/login.html');
 });
-exp.use('/client',express.static(__dirname + '/client'));
+exp.use(express.static(__dirname));
  
 serv.listen(8000);
 console.log("Server is running, it's gonna get away!");
