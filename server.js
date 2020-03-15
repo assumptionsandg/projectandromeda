0e68c6d14354dca91bae79a5b2b5fdb9c0ed70c3
assumptionsandg <jrhutchinson01@gmail.com>
2020-03-15 23:40:59 +0000

    Added login framework

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+var mongojs = require("mongojs");
+var db = mongojs('localhost:27017/projectAndromeda', ['account','progress']);
+
 var express = require('express');
 var exp = express();
 var serv = require('http').Server(exp);
+
+var io = require('socket.io').listen(8080)
 
 //server
 
@@ -11,3 +16,75 @@ exp.use(express.static(__dirname));
  
 serv.listen(8000);
 console.log("Server is running, it's gonna get away!");
+
+var isValidPassword = function(data,cb){
+    db.account.find({username:data.username,password:data.password},function(err,res){
+        if(res.length > 0)
+            cb(true);
+        else
+            cb(false);
+    });
+ }
+ 
+ var isUsernameTaken = function(data,cb){
+    db.account.find({username:data.username},function(err,res){
+        if(res.length > 0)
+            cb(true);
+        else
+            cb(false);
+    });
+ }
+    
+ var addUser = function(data,cb){
+    db.account.insert({username:data.username,password:data.password},function(err){
+        cb();
+    });
+ }
+ 
+ io.sockets.on('connection', function(socket){
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+   
+    socket.on('signIn',function(data){
+        isValidPassword(data,function(res){
+            if(res){
+                Player.onConnect(socket);
+                socket.emit('signInResponse',{success:true});
+            } else {
+                socket.emit('signInResponse',{success:false});         
+            }
+        });
+    });
+ 
+    socket.on('signUp',function(data){
+        isUsernameTaken(data,function(res){
+            if(res){
+                socket.emit('signUpResponse',{success:false});     
+            } else {
+                addUser(data,function(){
+                    socket.emit('signUpResponse',{success:true});                  
+                });
+            }
+        });    
+    });
+   
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id];
+        Player.onDisconnect(socket);
+    });
+ 
+    socket.on('sendMsgToServer',function(data){
+        var playerName = ("" + socket.id).slice(2,7);
+        for(var i in SOCKET_LIST){
+            SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
+        }
+    });
+   
+    socket.on('evalServer',function(data){
+        if(!DEBUG)
+            return;
+        var res = eval(data);
+        socket.emit('evalAnswer',res);     
+    });
+     
+ });
